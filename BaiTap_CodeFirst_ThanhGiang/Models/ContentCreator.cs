f6770b473cf0476409f96560c12363cc1313ff73
TTThanhGiang <t90adidas16@gmail.com>
2024-04-22 09:12:14 +0700

    Add project files.

---
+++ BaiTap_CodeFirst_ThanhGiang/Models/ContentCreator.cs
@@ -0,0 +1,12 @@
+﻿namespace BaiTap_CodeFirst_ThanhGiang.Models
+{
+    public class ContentCreator
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public int Age { get;set; }
+        public int SubscribersCount { get; set; }   
+        public DateTime JoinedData { get; set; }
+        public List<Video> videos { get; set; }
+    }
+}
